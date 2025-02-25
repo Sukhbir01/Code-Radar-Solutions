cadf47fd088d4f7101d1474dc2b925c729749072
Sukhbir Singh <sukhbirgame3@gmail.com>
2025-02-25 10:22:02 +0530

    Solution to Check Voting Eligibility by Age and Citizenship

---
+++ Check Voting Eligibility by Age and Citizenship@@6788dd63490637cfc44e1ba5/code.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if((a<=18) && (b=1 && b=0)){
+        printf("Prime");
+    }
+    else{
+        printf("Not Prime");
+    }
+    return 0;
+}
